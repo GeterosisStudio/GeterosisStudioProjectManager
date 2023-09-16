673eea1d254a8f01676ba055b11f8a08a5c1b38a
DESKTOP-EAHJTPT\AlexLip <lipchaksasha505@gmail.com>
2023-09-16 22:35:13 +0300

    add maya GSPM plugin for maya

--- GSMaya/MayaPlugin.py
+++ GSMaya/MayaPlugin.py
@@ -1,35 +1,59 @@
+# -------------------------------------------------------------------------------------------------------------------- #
+
 import sys
 import maya.OpenMaya as OpenMaya
 import maya.OpenMayaMPx as OpenMayaMPx
 
-kPluginCmdName = "spHelloWorld"
+nodeName = "GSPMSceneNode"
+nodeID = OpenMaya.MTypeId(0x100fff)
 
-# Command
-class scriptedCommand(OpenMayaMPx.MPxCommand):
+
+class GSPMSceneNode(OpenMayaMPx.MPxNode):
     def __init__(self):
-        OpenMayaMPx.MPxCommand.__init__(self)
+        OpenMayaMPx.MPxNode.__init__(self)
 
-    # Invoked when the command is run.
-    def doIt(self,argList):
-        print "Hello World!"
 
-# Creator
-def cmdCreator():
-    return OpenMayaMPx.asMPxPtr( scriptedCommand() )
+def nodeCreator():
+    return OpenMayaMPx.asMPxPtr(GSPMSceneNode())
 
-# Initialize the script plug-in
+
+def nodeInitializer():
+    owner = OpenMaya.MFnStringData().create("")
+    string_data_attribute = OpenMaya.MFnTypedAttribute()
+    GSPMSceneNode.owner = string_data_attribute.create("owner", "own", OpenMaya.MFnData.kString, owner)
+    GSPMSceneNode.addAttribute(GSPMSceneNode.owner)
+
+    project_name = OpenMaya.MFnStringData().create("")
+    string_data_attribute = OpenMaya.MFnTypedAttribute()
+    GSPMSceneNode.project_name = string_data_attribute.create("Project name", "prn", OpenMaya.MFnData.kString,
+                                                              project_name)
+    GSPMSceneNode.addAttribute(GSPMSceneNode.project_name)
+
+    project_path = OpenMaya.MFnStringData().create("")
+    string_data_attribute = OpenMaya.MFnTypedAttribute()
+    GSPMSceneNode.project_path = string_data_attribute.create("Project path", "prp", OpenMaya.MFnData.kString,
+                                                              project_path)
+    GSPMSceneNode.addAttribute(GSPMSceneNode.project_path)
+
+    scene_type = OpenMaya.MFnStringData().create("")
+    string_data_attribute = OpenMaya.MFnTypedAttribute()
+    GSPMSceneNode.scene_type = string_data_attribute.create("Scene type", "st", OpenMaya.MFnData.kString, scene_type)
+    GSPMSceneNode.addAttribute(GSPMSceneNode.scene_type)
+
+
 def initializePlugin(mobject):
-    mplugin = OpenMayaMPx.MFnPlugin(mobject)
+    mPlugin = OpenMayaMPx.MFnPlugin(mobject)
     try:
-        mplugin.registerCommand( kPluginCmdName, cmdCreator )
+        mPlugin.registerNode(nodeName, nodeID, nodeCreator, nodeInitializer)
     except:
-        sys.stderr.write( "Failed to register command: %sn" % kPluginCmdName )
-        raise
+        sys.stderr.write("Failed to register node: {}".format(nodeName))
 
-# Uninitialize the script plug-in
+
 def uninitializePlugin(mobject):
-    mplugin = OpenMayaMPx.MFnPlugin(mobject)
+    mPlugin = OpenMayaMPx.MFnPlugin(mobject)
     try:
-        mplugin.deregisterCommand( kPluginCmdName )
+        mPlugin.deregisterNode(nodeID)
     except:
-        sys.stderr.write( "Failed to unregister command: %sn" % kPluginCmdName )
+        sys.stderr.write("Failed to deregister node: {}".format(nodeName))
+
+#
