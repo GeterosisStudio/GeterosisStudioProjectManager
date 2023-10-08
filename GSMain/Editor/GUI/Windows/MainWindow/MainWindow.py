f6c8d59e90e455c6d197417e13ab89e8b120cfdf
DESKTOP-EAHJTPT\AlexLip <lipchaksasha505@gmail.com>
2023-10-08 21:51:41 +0300

    refact - remove unused file

--- GSMain/Editor/GUI/Windows/MainWindow/MainWindow.py
+++
@@ -1,6 +0,0 @@
-import sys
-from PySide6.QtWidgets import QApplication, QWidget
-q = QApplication(sys.argv)
-w = QWidget()
-w.show()
-sys.exit(q.exec())
--- main.py
+++
@@ -1,7 +0,0 @@
-from PyQt5 import QtWidgets, uic, QtCore
-import os
-import sys
-app = QtWidgets.QApplication(sys.argv)
-window = uic.loadUi("")
-os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1.5"
-app.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
