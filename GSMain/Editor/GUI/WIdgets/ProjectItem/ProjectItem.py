29dec57ed61d01715f6dddbbd9c847ebd277714d
DESKTOP-EAHJTPT\AlexLip <lipchaksasha505@gmail.com>
2023-10-14 21:23:47 +0300

    -add icon in project browser

--- GSMain/Editor/GUI/WIdgets/ProjectItem/ProjectItem.py
+++ GSMain/Editor/GUI/WIdgets/ProjectItem/ProjectItem.py
@@ -1,7 +1,7 @@
 import os
 import json
 from PySide6 import QtWidgets, QtUiTools
-
+from PySide6.QtGui import QIcon, QPixmap
 
 class ProjectItem(QtWidgets.QWidget):
 
@@ -12,6 +12,8 @@ class ProjectItem(QtWidgets.QWidget):
         self.layout = QtWidgets.QVBoxLayout()
         self.layout.addWidget(self.ui)
         self.setLayout(self.layout)
+
+        self.proj_ext = '.gsproj'
         self.project_path = self.set_project_path(project_path)
         self.project_file = self.set_project_file()
         self.project_info = self.load_project_info()
@@ -24,19 +26,19 @@ class ProjectItem(QtWidgets.QWidget):
         if os.path.isdir(project_path):
             for elem in os.listdir(project_path):
                 if os.path.isfile(os.path.join(project_path, elem)):
-                    if os.path.splitext(os.path.join(project_path, elem))[1] == '.gsproj':
+                    if os.path.splitext(os.path.join(project_path, elem))[1] == self.proj_ext:
                         if project_path[-1] != "/":
                             project_path += "/"
                         return project_path
 
         if os.path.isfile(project_path):
-            if os.path.splitext(project_path)[1] == '.gsproj':
+            if os.path.splitext(project_path)[1] == self.proj_ext:
                 return project_path.replace(project_path.split("/")[-1], "")
         return False
 
     def set_project_file(self):
         for elem in os.listdir(self.project_path):
-            if elem[-7:] == '.gsproj':
+            if elem[-7:] == self.proj_ext:
                 return elem
         return False
 
@@ -49,7 +51,16 @@ class ProjectItem(QtWidgets.QWidget):
         self.ui.project_name.setText(self.project_info["name"])
         self.ui.project_path.setText(self.project_path)
 
+        if self.project_info["image"]:
+            self.ui.project_icon
 
+            icon_label = QtWidgets.QLabel(self.ui.project_icon)
+            icon = QIcon(self.project_path + self.project_info["image"])
+            pixmap = icon.pixmap(60, 60)
+            icon_label.setPixmap(pixmap)
+
+
+"""
 import sys
 from PySide6.QtWidgets import QApplication
 
@@ -60,3 +71,5 @@ else:
 w = ProjectItem("E:/Projects/ILLUSION_1")
 w.show()
 sys.exit(q.exec())
+"""
+
