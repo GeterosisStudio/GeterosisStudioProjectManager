40b120afe102ba67b3d00916df1c01f29733e63f
DESKTOP-EAHJTPT\AlexLip <lipchaksasha505@gmail.com>
2023-12-01 20:35:28 +0300

    minor commit

---
+++ Settings/Configs/AssetTypes.py
@@ -0,0 +1,13 @@
+import json
+from Plugins.GSMain import Log
+from Settings import Enviroment
+
+
+PROJECT_MANAGER_PATH = Enviroment.GSPM_PATH
+def get_all_types():
+    try:
+        with open(PROJECT_MANAGER_PATH + 'Settings/AssetTypes.gsconfig') as asset_type_config_file:
+            asset_type_config = json.load(asset_type_config_file)
+        return asset_type_config
+    except Exception as e:
+        Log.warning(str(e))
